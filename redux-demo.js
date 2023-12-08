5f21d1e7f5a75faef0d0a5d1306c474ab5cd65a0
SO HUI <haejuuun925@gmail.com>
2023-12-08 20:33:45 +0900

    feat: redux 기본 개념 학습

---
+++ redux-demo.js
@@ -0,0 +1,28 @@
+// 기본 노드 import 구문
+const redux = require('redux');
+
+// Reducer 함수
+                    // 현재 상태, 액션
+const counterReducer = (state = { counter: 0 }, action) => {
+    // 새로운 상태 return
+    return {
+        counter: state.counter + 1
+    };
+};
+
+// 저장소 생성
+const store = redux.createStore(counterReducer);
+
+// 구독
+const counterSubscriber = () => {
+    // getState(): createStore()로 생성된 저장소에서 사용할 수 있는 메소드.
+    // 업데이트된 후 최신 상태의 스냅샷 제공.
+    const latestState = store.getState();
+    console.log(latestState);
+};
+
+// 개발자가 실행하는 것이 아닌 리덕스가 실행해줌.
+store.subscribe(counterSubscriber);
+
+// action(자바스크립트 객체) 발송
+store.dispatch({ type: 'increment' });
